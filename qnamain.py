01ef79d3bc4c5e2e4ecc4ca13272d8365e13da63
SilentVOEZ <65421364+SilentVOEZ@users.noreply.github.com>
2020-08-02 16:49:36 +0800

    Add files via upload

--- qnamain.py
+++ qnamain.py
@@ -1,10 +1,14 @@
-# QnA Bot Version 0.9.1 Pre-release by SilentVOEZ#2523
+# QnA Bot Version 0.9.2 Pre-release by SilentVOEZ#2523
 
-import discord
+import discord, datetime, time
 import os
+#import sys
+import logging
 
 import asyncio
 from discord.ext import commands
+
+start_time = time.time()
 
 TOKEN = open("./token.txt","r").read()
 PREFIX = open("./prefix.txt","r").read()
@@ -26,6 +30,21 @@ async def on_ready():
     activity = discord.Game(name="q!help, Pre-release", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
 
+# Uptime command
+@bot.command(pass_context=True)
+async def uptime(ctx):
+    current_time = time.time()
+    difference = int(round(current_time - start_time))
+    text = str(datetime.timedelta(seconds=difference))
+    embed = discord.Embed(colour=ctx.message.author.top_role.colour)
+    embed.add_field(name="Uptime", value=text)
+    embed.set_footer(text="Bot by SilentVOEZ")
+    try:
+        await ctx.send(embed=embed)
+    except discord.HTTPException:
+        await ctx.send("Current uptime: " + text)
+
+
 # Status Cycle (TY DaijobuDes)
 @bot.command()
 @commands.is_owner()
@@ -45,42 +64,46 @@ async def status(ctx, status: str):
 @bot.command()
 @commands.is_owner()
 async def load(ctx, extension):
+    consoletime = datetime.datetime.now()
     try:
         bot.load_extension(f'plugins.{extension}')
-        print(f'{extension} loaded')
+        print(f'{consoletime} [INFO] {extension} loaded')
         await ctx.send(f'**{extension}** loaded')
     except Exception as e:
-        print(f"Failed to load {extension}. Please check the extension's code or extension does not exist.")
+        print(f"{consoletime} [WARNING] Failed to load {extension}. Please check the extension's code or extension does not exist.")
         await ctx.send(f"Failed to load **{extension}**. Please check the extension's code or extension does not exist.")
         raise e
 
 @bot.command()
 @commands.is_owner()
 async def unload(ctx, extension):
+    consoletime = datetime.datetime.now()
     try:
         bot.unload_extension(f'plugins.{extension}')
-        print(f'{extension} unloaded')
+        print(f'{consoletime} [INFO] {extension} unloaded')
         await ctx.send(f'**{extension}** unloaded')
     except Exception as e:
-        print(f"Failed to unload {extension}. Extension does not exist or it's been already unloaded.")
+        print(f"{consoletime} [WARNING] Failed to unload {extension}. Extension does not exist or it's been already unloaded.")
         await ctx.send(f"Failed to unload **{extension}**. Extension does not exist or it's been already unloaded.")
         raise e
 @bot.command()
 @commands.is_owner()
 async def reload(ctx, extension):
+    consoletime = datetime.datetime.now()
     try:
         bot.unload_extension(f'plugins.{extension}')
         bot.load_extension(f'plugins.{extension}')
-        print(f'{extension} reloaded')
+        print(f'{consoletime} [INFO] {extension} reloaded')
         await ctx.send(f'**{extension}** reloaded')
     except Exception as e:
-        print(f"Failed to reload {extension}. Please check the extension's code or you were trying reloading an unloaded extension or non-exisiting extension.")
+        print(f"{consoletime} [WARNING] Failed to reload {extension}. Please check the extension's code or you were trying reloading an unloaded extension or non-exisiting extension.")
         await ctx.send(f"Failed to reload **{extension}**. Please check the extension's code or you were trying reloading an unloaded extension or non-exisiting extension.")
         raise e
 
 for filename in os.listdir('./plugins'):
+    consoletime = datetime.datetime.now()
     if filename.endswith('.py'):
         bot.load_extension(f'plugins.{filename[:-3]}')
-        print(f'{filename} loaded')
+        print(f'{consoletime} [INFO] {filename} loaded')
 
 bot.run(TOKEN)
